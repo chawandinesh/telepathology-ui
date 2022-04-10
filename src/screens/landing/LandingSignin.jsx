80f1ae7724555bb5df83e9dc31684c5bc5f41e1e
dinesh <chavandinesh53@gmail.com>
2022-04-10 09:03:30 +0530

    added ui changes in landing signin

--- src/screens/landing/LandingSignin.jsx
+++ src/screens/landing/LandingSignin.jsx
@@ -7,34 +7,34 @@ const LandingSignin = () => {
   return (
     <div className="landing__container">
       <div className="landing__header">
-        <span>AI Based Telepathology</span> 
+        <span className="b">AI Based Telepathology</span> 
       </div>
-      <hr className="landing__hr" />
+      <hr className="landing__hr bgb" />
       <div className="signin__container">
         <div className="landing__img">
 
         <img src={abc} alt="bgimg" height="350px" width="100%" />
         </div>
        <div className="signin__patient">
-           <h3 className="text-white font-weight-bold">For Patients</h3>
-           <hr className="landing__hr" />
-           <p>
+           <h3 className="font-weight-bold b">For Patients</h3>
+           <hr className="landing__hr bgb" />
+           <p className="b">
             A patient is any recipient of health care services that are performed by healthcare professionals. 
             </p>
            <div className="landing__signin__container">
-               <button className="landing__signin" onClick={() => navigate("/patient/login")}>
+               <button className="landing__signin bgb text-white" onClick={() => navigate("/patient/login")}>
                    Login as patient
                </button>
            </div>
        </div>
        <div className="signin__pathologist">
-         <h3 className="text-white font-weight-bold">For Pathologist</h3>
-         <hr className="landing__hr" />
-         <p>
+         <h3 className="font-weight-bold b">For Pathologist</h3>
+         <hr className="landing__hr bgb" />
+         <p className="b">
             The role of pathology is to detect and determine the causes of infections and disease.
            </p>
          <div className="landing__signin__container">
-               <button className="landing__signin" onClick={() => navigate("/pathologist/login")}>
+               <button className="landing__signin bgb text-white" onClick={() => navigate("/pathologist/login")}>
                    Login as pathologist
                </button>
            </div>
